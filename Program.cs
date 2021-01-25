f944f386399ddd294a976db7af6e2c9fec6bf21b
gitstrato <gitstrato@4by.eu>
2021-01-25 10:16:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResstApiCallX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
